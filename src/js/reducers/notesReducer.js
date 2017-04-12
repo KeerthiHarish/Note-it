33a4a90bc9d76f107b29b9baafc8e03523a8f863
Keerthi harish <keerthi.harish10@gmail.com>
2017-04-13 02:17:35 +0530

    Enhance search by including tags

--- src/js/reducers/notesReducer.js
+++ src/js/reducers/notesReducer.js
@@ -2,15 +2,9 @@ export default function reducer(state={
   notes: [
     {
       id: 100,
-      title: 'First title',
-      message: 'First description',
+      title: 'Dummy title',
+      message: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit. There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain...',
       tags: ['one', 'two']
-    },
-    {
-      id: 101,
-      title: 'Second title',
-      message: 'Second description',
-      tags: ['xxx', 'yyy']
     }
   ],
   filterValue: null,
@@ -21,11 +15,29 @@ export default function reducer(state={
       return {...state, notes: [...state.notes, action.payload]}
     break;
     case 'FILTER_NOTE':
+    let filteredNotes = [];
+    let filterValue = action.payload.filterValue;
+    state.notes.map((note) => {
+      if(note.title.toLowerCase().includes(filterValue)) {
+        filteredNotes.push(note)
+      } else {
+        note.tags.map((tag) => {
+          if(tag.toLowerCase().includes(filterValue)) {
+            (filteredNotes.indexOf(note) == -1) && filteredNotes.push(note)
+          }
+        })
+      }
+    })
       return {
         ...state,
-        filteredNotes: [...state.notes.filter((note) => note.title.toLowerCase().includes(action.payload.filterValue))],
+        filteredNotes: [...filteredNotes],
         filterValue: action.payload.filterValue
       }
+      // return {
+      //   ...state,
+      //   filteredNotes: [...state.notes.filter((note) => note.title.toLowerCase().includes(action.payload.filterValue))],
+      //   filterValue: action.payload.filterValue
+      // }
     break;
     case 'DELETE_NOTE':
       let index = state.notes.findIndex(note => note.id == action.payload.noteId)
